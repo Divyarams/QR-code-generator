a40c0a7920e1035912a4be839c0237070e90b33b
Divyarams <92635226+Divyarams@users.noreply.github.com>
2022-04-05 11:39:32 +0530

    Update qrcode.py

--- qrcode.py
+++ qrcode.py
@@ -3,22 +3,23 @@ import pyqrcode
 import boto3
 import uuid
 import json
+import os
 from pyqrcode import QRCode
 
 s3 = boto3.client('s3')
 
 def img_process():
     
-    source_Bucket_name = 'SOURCE_BUCKET'
-    object_name = 'OBJECY_KEY'
-    Dest_Bucket_name = 'DEST_BUCKET'
+    source_Bucket_name = os.environ['SourceBucket']
+    object_name = os.environ['ObjectName']
+    Dest_Bucket_name = os.environ['DestinationBucket']
 
     
     
-    s = 'https://{0}.s3.amazonaws.com/{1}'.format (Bucket_name,object_name)
+    s = 'https://{0}.s3.amazonaws.com/{1}'.format (source_Bucket_name,object_name)
     url = pyqrcode.create(s)        
-    url.svg(" file.svg", scale = 8)
-    with open("file.svg", "rb") as f:
+    url.svg(" /tmp/file.svg", scale = 8)
+    with open("/tmp/file.svg", "rb") as f:
         s3.upload_fileobj(f, Dest_Bucket_name, "{}.svg".format (object_name))
     return True
 
